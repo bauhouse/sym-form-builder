91f31f65a9117bc502bdf6c4d6720813e00152f5
Stephen Bau <bauhouse@gmail.com>
2010-03-13 10:22:14 -0800

    Modify the Save Project User event according to the EventEx extension instructions

--- workspace/events/event.save_project_user.php
+++ workspace/events/event.save_project_user.php
@@ -1,6 +1,6 @@
 <?php
 
-	require_once(TOOLKIT . '/class.event.php');
+	require_once(EXTENSIONS . '/eventex/lib/class.eventex.php');
 	
 	Class eventsave_project_user extends Event{
 		
@@ -23,50 +23,14 @@
 		}
 
 		public static function getSource(){
-			return '2';
+			return array("projects", "users");
 		}
 
-		public static function allowEditorToParse(){
-			return true;
-		}
-
+		// When the documentation method is not included, viewing the event results in a blank page
 		public static function documentation(){
 			return '
-        <h3>Success and Failure XML Examples</h3>
-        <p>When saved successfully, the following XML will be returned:</p>
-        <pre class="XML"><code>&lt;save-project-user result="success" type="create | edit">
-  &lt;message>Entry [created | edited] successfully.&lt;/message>
-&lt;/save-project-user></code></pre>
-        <p>When an error occurs during saving, due to either missing or invalid fields, the following XML will be returned:</p>
-        <pre class="XML"><code>&lt;save-project-user result="error">
-  &lt;message>Entry encountered errors when saving.&lt;/message>
-  &lt;field-name type="invalid | missing" />
-  ...
-&lt;/save-project-user></code></pre>
-        <h3>Example Front-end Form Markup</h3>
-        <p>This is an example of the form markup you can use on your frontend:</p>
-        <pre class="XML"><code>&lt;form method="post" action="" enctype="multipart/form-data">
-  &lt;input name="MAX_FILE_SIZE" type="hidden" value="5242880" />
-  &lt;label>Title
-    &lt;input name="fields[title]" type="text" />
-  &lt;/label>
-  &lt;label>Description
-    &lt;textarea name="fields[description]" rows="15" cols="50">&lt;/textarea>
-  &lt;/label>
-  &lt;label>Status
-    &lt;select name="fields[status]">
-      &lt;option value="Active">Active&lt;/option>
-      &lt;option value="Inactive">Inactive&lt;/option>
-      &lt;option value="Finished">Finished&lt;/option>
-    &lt;/select>
-  &lt;/label>
-  &lt;input name="fields[contributors]" type="hidden" value="..." />
-  &lt;input name="action[save-project-user]" type="submit" value="Submit" />
-&lt;/form></code></pre>
-        <p>To edit an existing entry, include the entry ID value of the entry in the form. This is best as a hidden field like so:</p>
-        <pre class="XML"><code>&lt;input name="id" type="hidden" value="23" /></code></pre>
-        <p>To redirect to a different location upon a successful save, include the redirect location in the form. This is best as a hidden field like so, where the value is the URL to redirect to:</p>
-        <pre class="XML"><code>&lt;input name="redirect" type="hidden" value="http://home/sym/form-builder/success/" /></code></pre>';
+        <h3>An EventEx Event</h3>
+        <p>The purpose of which is to demonstrate the ability to save to multiple entries using the EventEx extension.</p>';
 		}
 		
 		public function load(){			
