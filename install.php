f07dacadbb4d4886fab9c5205090d0f11543295d
bauhouse <bauhouse@gmail.com>
2010-03-24 10:16:37 -0700

    Change sitename configuration setting to Form Builder

--- install.php
+++ install.php
@@ -198,7 +198,7 @@
 		$conf['symphony']['pages_table_nest_children'] = 'yes';
 		$conf['log']['archive'] = '1';
 		$conf['log']['maxsize'] = '102400';
-		$conf['general']['sitename'] = 'Symphony CMS';
+		$conf['general']['sitename'] = 'Form Builder';
 		$conf['image']['cache'] = '1';
 		$conf['image']['quality'] = '90';
 		$conf['database']['driver'] = 'mysql';
